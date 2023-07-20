357b8cba4b8fc9de8a76f861a0bc9a7728fb552f
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-07-20 11:42:52 -0300

    Vídeo 1.2

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,18 @@
 package br.com.alura.screenmatch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Primeiro projeto Spring sem Web");
+	}
 }
