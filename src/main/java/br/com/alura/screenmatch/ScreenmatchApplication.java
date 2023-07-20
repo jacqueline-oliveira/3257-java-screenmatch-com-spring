e34f2b4f931e64b8b0928e894e5125b6961eea7a
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-07-20 11:39:17 -0300

    criação do projeto Spring Initializr

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+}
