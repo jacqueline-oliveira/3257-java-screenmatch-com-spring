50cef4fe9c81b01f93a878097784a7f9159cdbf6
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-07-24 11:35:07 -0300

    Vídeo 3.3

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -3,6 +3,7 @@ package br.com.alura.screenmatch.principal;
 import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
@@ -63,5 +64,11 @@ public class Principal {
                .limit(5)
                .forEach(System.out::println);
 
+       List<Episodio> episodios = temporadas.stream()
+               .flatMap(t -> t.episodios().stream()
+                       .map(d -> new Episodio(t.numero(), d)))
+               .collect(Collectors.toList());
+
+       episodios.forEach(System.out::println);
     }
 }
