07c1241a67d2f9d077f6f1495e3c41ced79be7e3
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-07-24 11:51:22 -0300

    Vídeo 3.2

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -7,9 +7,10 @@ import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -21,34 +22,46 @@ public class Principal {
     private final String API_KEY = "&apikey=6585022c";
 
     public void exibeMenu(){
-//        System.out.println("Digite o nome da série para busca");
-//        var nomeSerie = leitura.nextLine();
-//        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
-//        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-//        System.out.println(dados);
-//
-//        List<DadosTemporada> temporadas = new ArrayList<>();
-//
-//        for (int i = 1; i<=dados.totalTemporadas(); i++){
-//            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") +"&season=" + i + API_KEY);
-//            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-//            temporadas.add(dadosTemporada);
+        System.out.println("Digite o nome da série para busca");
+        var nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for (int i = 1; i<=dados.totalTemporadas(); i++){
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") +"&season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
+
+//        for(int i = 0; i < dados.totalTemporadas(); i++){
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for(int j = 0; j< episodiosTemporada.size(); j++){
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
 //        }
-//        temporadas.forEach(System.out::println);
-//
-////        for(int i = 0; i < dados.totalTemporadas(); i++){
-////            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-////            for(int j = 0; j< episodiosTemporada.size(); j++){
-////                System.out.println(episodiosTemporada.get(j).titulo());
-////            }
-////        }
-//
-//        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
 
-        List<String> nomes = Arrays.asList("Jacque", "Iasmin", "Paulo", "Rodrigo", "Nico");
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
 
-        nomes.stream().sorted().limit(3).filter(n -> n.startsWith("N")).map(n -> n.toUpperCase())
+//        List<String> nomes = Arrays.asList("Jacque", "Iasmin", "Paulo", "Rodrigo", "Nico");
+//
+//        nomes.stream().sorted().limit(3).filter(n -> n.startsWith("N")).map(n -> n.toUpperCase())
+//                .forEach(System.out::println);
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTop 5 episódios");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
                 .forEach(System.out::println);
+
 
     }
 }
